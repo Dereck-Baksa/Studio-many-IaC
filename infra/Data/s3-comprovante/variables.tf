ab9134bd06e49b700a71ffb9d6d92f1dc1fc5ef5
Dereck-baksa <dereckmbaksa@gmail.com>
2026-09-06 19:41:02 -0300

    s3-comprovante

--- infra/Data/s3-comprovante/variables.tf
+++ infra/Data/s3-comprovante/variables.tf
@@ -1,19 +1,17 @@
-variable "instance_type" {
-  description = "WebServer EC2 instance type"
+variable "bucket_name" {
+  description = s3-comprovante-${random_string.sufixo.result}
   type        = string
-  default     = "t3.micro"
-  validation {
-    condition     = contains(["t3.micro", "t3.small", "t3.medium"], var.instance_type)
-    error_message = "Tipo da instancia deve ser t3.micro, t3.small ou t3.medium."
-  }
 }
 
-variable "my_ip" {
-  description = "IP address formato em CIDR (e.g. 203.0.113.1/32)."
+variable "region" {
+  description = "Região da AWS"
   type        = string
-  default     = "0.0.0.0/0"
-  validation {
-    condition     = can(regex("^(\\d{1,3}\\.){3}\\d{1,3}\\/\\d{1,2}$", var.my_ip))
-    error_message = "Deve ser um IP CIDR válido na forma de x.x.x.x/x."
-  }
+  default     = "us-east-1"
+}
+
+
+variable "key" {
+  description = "key das pastas"
+  type        = string
+  default     = "pendentes/"
 }
